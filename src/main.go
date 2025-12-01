2bad627bc70b2a63c6e861c13c2c11c1bb930a77
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2025-12-01 14:00:58 +0200

    Main executable and its source with comments

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package forum
+
+func Main(args []string) {
+	// maybe initialize something first if needed...
+	// for example the database?!?!
+	// maybe that could be a flag...
+	// e.g. --init
+
+	// in case the flag is not passed, the auto intialization could be triggered
+
+	// obviously start the HTTP server, maybe optionally we could set 2 things:
+	// ip
+	// port
+	// startServer(ip, port)
+
+	// dunno... thoughts?!
+}
