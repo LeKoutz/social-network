0821d37287837753ad4235289293a8dab15e96e8
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2025-12-02 20:56:37 +0200

    Updates README.md and provides usage screen

--- src/main.go
+++ src/main.go
@@ -1,5 +1,19 @@
 package forum
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func usage(programName string) {
+	fmt.Fprintf(os.Stderr, "Usage:\n\n")
+	fmt.Fprintf(os.Stderr, "./%s [ip] [port]\n", programName)
+	fmt.Fprintf(os.Stderr, "./%s [port]\n", programName)
+	fmt.Fprintf(os.Stderr, "./%s\n", programName)
+}
+
 // Entry point for the program
 func Main(args []string) {
 	// TODO pass args and populate them maybe, otherwise default
@@ -7,18 +21,23 @@ func Main(args []string) {
 	// for example the database?!?!
 	// maybe that could be a flag...
 	// e.g. --init
-
 	var ip, port string
 	// in case the flag is not passed, the auto intialization could be triggered
-	if len(args) != 3 {
+	if len(args) == 3 {
+		ip = args[1]
+		port = args[2]
+	} else if len(args) == 2 {
+		ip = "127.0.0.1"
+		port = args[1]
+	} else if len(args) == 1 {
 		ip = "127.0.0.1"
 		port = "8080"
 	} else {
-		ip = args[1]
-		port = args[2]
+		programName := filepath.Base(args[0])
+		usage(programName)
+		os.Exit(1)
 	}
-
+	log.Printf("http://%s:%s/", ip, port)
 	// TODO Initialization of needed stuff
-
 	startServer(ip, port) // Unused values...
 }
