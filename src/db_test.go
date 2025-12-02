33f16811e00b56bcb371e35db4cd1edb5330481a
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2025-12-02 16:21:53 +0200

    Very basic db stuff: just the connection

---
+++ src/db_test.go
@@ -0,0 +1,24 @@
+package forum
+
+import "testing"
+
+func TestInit(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "Test",
+			want: "mpla",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Init()
+			if err != nil {
+				t.Logf("%s", err.Error())
+			}
+
+		})
+	}
+}
