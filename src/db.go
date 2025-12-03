241e5fc17a897a0fd4765b132ea61022dff202d8
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2025-12-03 18:28:07 +0200

    Inits DB and creates tables

--- src/db.go
+++ src/db.go
@@ -14,11 +14,103 @@ func Init() error {
 		return err
 	}
 	defer db.Close()
-	var sqliteVersion string
-	err = db.QueryRow("select sqlite_version()").Scan(&sqliteVersion)
+	// var sqliteVersion string
+	// err = db.QueryRow("select sqlite_version()").Scan(&sqliteVersion)
+	// if err != nil {
+	// 	return err
+	// }
+	_, err = db.Exec(createUsersTable())
 	if err != nil {
 		return err
 	}
-	fmt.Fprintf(os.Stderr, "%s", sqliteVersion)
+	_, err = db.Exec(createPostsTable())
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(createCategoriesTable())
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(createCommentsTable())
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(createReactionsTable())
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(createPostsCategoriesTable())
+	if err != nil {
+		return err
+	}
+	// fmt.Fprintf(os.Stderr, "%s", sqliteVersion)
 	return nil
+}
+
+func createUsersTable() string {
+	return `CREATE TABLE IF NOT EXISTS "users" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"email"	TEXT NOT NULL UNIQUE,
+		"username"	TEXT NOT NULL,
+		"salt"	TEXT UNIQUE,
+		"hash"	TEXT,
+		"session_key"	TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	)`
+}
+
+func createPostsTable() string {
+	return `CREATE TABLE IF NOT EXISTS "posts" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"title"	TEXT,
+		"body"	TEXT,
+		"user_id"	INTEGER NOT NULL,
+		PRIMARY KEY("id" AUTOINCREMENT),
+		FOREIGN KEY("user_id") REFERENCES "users"("id")
+	)`
+}
+
+func createCategoriesTable() string {
+	return `CREATE TABLE IF NOT EXISTS "categories" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"name"	TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	)`
+}
+
+func createCommentsTable() string {
+	return `CREATE TABLE IF NOT EXISTS "comments" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"post_id"	INTEGER NOT NULL,
+		"user_id"	INTEGER NOT NULL,
+		"body"	TEXT NOT NULL,
+		PRIMARY KEY("id" AUTOINCREMENT),
+		FOREIGN KEY("post_id") REFERENCES "posts"("id"),
+		FOREIGN KEY("user_id") REFERENCES "users"("id")
+	)`
+}
+
+func createPostsCategoriesTable() string {
+	return `CREATE TABLE IF NOT EXISTS "posts_categories" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"post_id"	INTEGER NOT NULL,
+		"category_id"	INTEGER NOT NULL,
+		PRIMARY KEY("id" AUTOINCREMENT),
+		FOREIGN KEY("category_id") REFERENCES "categories"("id"),
+		FOREIGN KEY("post_id") REFERENCES "posts"("id")
+	)`
+}
+
+func createReactionsTable() string {
+	return `CREATE TABLE IF NOT EXISTS "reactions" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"post_id"	INTEGER,
+		"user_id"	INTEGER NOT NULL,
+		"comment_id"	INTEGER,
+		"value"	INTEGER NOT NULL,
+		PRIMARY KEY("id" AUTOINCREMENT),
+		FOREIGN KEY("comment_id") REFERENCES "comments"("id"),
+		FOREIGN KEY("post_id") REFERENCES "posts"("id"),
+		FOREIGN KEY("user_id") REFERENCES "users"("id")
+	)`
 }
