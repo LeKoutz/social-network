849018c0fb516c54d3533c8ca1248a82ed554400
cktistak <ktistakis.c@gmail.com>
2025-12-06 16:20:51 +0200

    basic errors for comments

--- src/errors.go
+++ src/errors.go
@@ -21,6 +21,8 @@ var (
 	ErrorPostTitleEmpty        = errors.New("Post title can't be empty.")
 	ErrorPostHasNoCategory     = errors.New("Post category can't be empty.")
 	ErrorPostPermissionDenied  = errors.New("You must be logged in to create a post.")
+	ErrorCommentEmpty          = errors.New("Comment can't be empty.")
+	ErrorCommentTooLong		   = errors.New("Comment is too long.")
 	ErrorCategoryEmptyId       = errors.New("Category ID can't be empty.")
 	ErrorCategoryAlreadyExists = errors.New("Category already exists.")
 	ErrorCategoryNameEmpty     = errors.New("Category name can't be empty.")
