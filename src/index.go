5fcd3789023f41dbab0d10568454eef44c5a511f
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2025-12-11 19:04:14 +0200

    Fixes previously broken index_view

--- src/index.go
+++ src/index.go
@@ -4,5 +4,12 @@ import "net/http"
 
 func showIndex(res http.ResponseWriter, _ *http.Request, user User) {
 	data := ResponseStruct{}
-	data.Init().SetUser(user).SetView("index_view").WriteResponse(res)
+	data.Init().SetUser(user)
+	categories, err := getAllCategories()
+	if err != nil {
+		(&Error{}).Consume(err).LogAndRespondError(res, user)
+		return
+	}
+	data.SetCategories(categories)
+	data.SetView("index_view").WriteResponse(res)
 }
