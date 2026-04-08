c3a06bf6a86afb946dda57ed17a7771da95d36a2
cktistak <student@172.16.10.225>
2026-04-08 21:59:45 +0300

    added sexy in-memory DB

--- src/models/db_test.go
+++ src/models/db_test.go
@@ -7,20 +7,17 @@ import (
 func TestInit(t *testing.T) {
 	tests := []struct {
 		name string
-		want string
 	}{
 		{
-			name: "Test",
-			want: "mpla",
+			name: "in-memory database",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := InitDB("./data/db.db")
+			err := InitDB(":memory:")
 			if err != nil {
-				t.Logf("%s", err.Error())
+				t.Errorf("InitDB failed: %v", err)
 			}
-
 		})
 	}
 }
