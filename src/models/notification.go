fdb7c56ace9d01233605506f80b31f42ce51b226
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-04-21 16:53:58 +0300

    Expands models with Notification struct and CreateNotification()

---
+++ src/models/notification.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+)
+
+type Notification struct {
+	Id        int64
+	UserId    int64
+	ActorId   int64
+	Actor     User
+	Type      string
+	TargetId  int64
+	Timestamp string
+	Read      bool
+}
+
+func CreateNotification(notification *Notification) error {
+	query := `INSERT INTO notifications (user_id, actor_id, type, target_id, timestamp) VALUES (?, ?, ?, ?, ?)`
+	_, err := DB.Exec(query, notification.UserId, notification.ActorId, notification.Type, notification.TargetId, notification.Timestamp)
+	return err
+}
