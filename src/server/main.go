6d330bb6d56bba711e6bd3de0944e2755a761eca
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2026-04-26 03:10:32 +0300

    Introduces Configuration type for argument parsing

--- src/server/main.go
+++ src/server/main.go
@@ -3,56 +3,70 @@ package server
 import (
 	"fmt"
 	"forum/src/models"
+	"forum/src/utils"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-func usage(programName string) {
-	fmt.Fprintf(os.Stderr, "Usage:\n\n")
-	fmt.Fprintf(os.Stderr, "./%s [--db-path <path>] [ip] [port]\n", programName)
-	fmt.Fprintf(os.Stderr, "./%s [--db-path <path>] [port]\n", programName)
-	fmt.Fprintf(os.Stderr, "./%s [--db-path <path>]\n", programName)
+func usage(programName string, toStdErr bool) {
+	fd := os.Stderr
+	if !toStdErr {
+		fd = os.Stdout
+	}
+	fmt.Fprintf(fd, "Usage:\n\n")
+	fmt.Fprintf(fd, "\t%s [--help]\n", programName)
+	fmt.Fprintf(fd, "\t%s [--db-path <path>] [ip] [port]\n", programName)
+	fmt.Fprintf(fd, "\t%s [--db-path <path>] [port]\n", programName)
+	fmt.Fprintf(fd, "\t%s [--db-path <path>]\n", programName)
 }
 
 func Main(args []string) {
-	var dbPath string = "./db.db"
-	var ip, port string
+	config := utils.DefaultConfiguration()
+
+	var positionalArgs []string
+
 	for i := 1; i < len(args); i++ {
-		if args[i] == "--db-path" && i+1 < len(args) {
-			dbPath = args[i+1]
+		switch args[i] {
+		case "--db-path":
+			config.DbPath = args[i+1]
 			i++
+		case "--help", "-h":
+			programName := filepath.Base(args[0])
+			usage(programName, false)
+			os.Exit(0)
+		case "--version":
+			programName := filepath.Base(args[0])
+			version := utils.GetVersion()
+			fmt.Println(programName+"-"+version)
+			os.Exit(0)
+		default:
+			positionalArgs = append(positionalArgs, args[i])
 		}
 	}
-	if err := models.InitDB(dbPath); err != nil {
+
+	if len(positionalArgs) == 2 {
+		config.Ip = positionalArgs[0]
+		config.Port = positionalArgs[1]
+	} else if len(positionalArgs) == 1 {
+		config.Port = positionalArgs[0]
+	} else if len(positionalArgs) == 0 {
+		// keep defaults
+	} else {
+		// unexpected argument length
+		programName := filepath.Base(args[0])
+		usage(programName, true)
+		os.Exit(1)
+	}
+
+	if err := models.InitDB(config.DbPath); err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		os.Exit(1)
 	}
 
-	var positionalArgs []string
-	for i := 1; i < len(args); i++ {
-		if args[i] == "--db-path" {
-			i++
-		} else {
-			positionalArgs = append(positionalArgs, args[i])
-		}
-	}
-	if len(positionalArgs) == 2 {
-		ip = positionalArgs[0]
-		port = positionalArgs[1]
-	} else if len(positionalArgs) == 1 {
-		ip = "127.0.0.1"
-		port = positionalArgs[0]
-	} else if len(positionalArgs) == 0 {
-		ip = "127.0.0.1"
-		port = "8080"
-	} else {
-		programName := filepath.Base(args[0])
-		usage(programName)
-		os.Exit(1)
-	}
-	log.Printf("http://%s:%s/", ip, port)
-	if err := startServer(ip, port); err != nil {
+	log.Printf("http://%s:%s/", config.Ip, config.Port)
+
+	if err := startServer(config.Ip, config.Port); err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		os.Exit(1)
 	}
