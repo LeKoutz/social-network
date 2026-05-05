90435ba6a9667df8958d52afbb22d160dc6b7a4a
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-05-05 17:32:36 +0300

    Refactors with parseCategoryId() for consistency

--- src/controllers/categories.go
+++ src/controllers/categories.go
@@ -7,6 +7,18 @@ import (
 	"strings"
 )
 
+func parseCategoryId(data models.ResponseStruct) (int64, error) {
+	id, ok := strings.CutPrefix(data.Request.RequestURI, "/category/view/")
+	if !ok || len(id) == 0 {
+		return 0, models.ErrorCategoryEmptyId
+	}
+	categoryId, err := utils.StringToInt64(id)
+	if err != nil {
+		return 0, models.ErrorInvalidCategoryId
+	}
+	return categoryId, nil
+}
+
 func showCategories(data models.ResponseStruct) {
 	categories, err := models.GetAllCategories()
 	if err != nil {
@@ -18,25 +30,19 @@ func showCategories(data models.ResponseStruct) {
 }
 
 func showCategory(data models.ResponseStruct) {
-	id, ok := strings.CutPrefix(data.Request.RequestURI, "/category/view/")
-	if !ok || len(id) == 0 {
-		(&models.Error{}).Consume(models.ErrorCategoryEmptyId).LogAndRespondError(data.Response, data.User)
-		return
-	}
-	id_int, err := utils.StringToInt64(id)
+	categoryId, err := parseCategoryId(data)
 	if err != nil {
-		err = models.ErrorInvalidCategoryId
 		(&models.Error{}).Consume(err).LogAndRespondError(data.Response, data.User)
 		return
 	}
-	category, err := models.GetCategoryById(id_int)
+	category, err := models.GetCategoryById(categoryId)
 	if err != nil {
 		(&models.Error{}).Consume(err).LogAndRespondError(data.Response, data.User)
 		return
 	}
 	data.Categories = models.Categories{}
 	data.Categories = append(data.Categories, category)
-	posts, err := models.GetPostsByCategoryId(id_int)
+	posts, err := models.GetPostsByCategoryId(categoryId)
 	if err != nil {
 		(&models.Error{}).Consume(err).LogAndRespondError(data.Response, data.User)
 		return
