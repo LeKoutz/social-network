f2d7e7459273d06b9cd3a080a3c27be499fddc86
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2026-06-24 20:51:19 +0300

    Home: Show text when user is logged in

--- public/js/app.js
+++ public/js/app.js
@@ -10,10 +10,7 @@ function mainViewLayout(data) {
         -->
         <div class="container">
             <div class="welcome">
-                <!--
-                    TODO: This should be derived from the API call
-                    <h2>Welcome{{if .User.LoggedIn}}, {{.User.Username}}{{end}}!</h2>
-                -->
+                <h2>Welcome${(data.User.LoggedIn)? data.User.Username:''}!</h2>
                 <p>Below, you will see the post categories available as well as a brief
                 description when applicable. Click any of them to navigate to its list
                 of posts.</p>
