d65c391d4560c43c35beef9d33fc7b6e008c8895
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2026-07-07 17:29:38 +0300

    Exports router logic to its own file

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,6 @@
 import { TopBar } from './topbar.js';
-import { showUserLogin, attachLoginListener } from './forms/user_login.js';
-import { showUserRegister, attachRegisterListener } from './forms/user_register.js';
-import { showPostCreateView, attachPostCreateListener, showPostEditView } from './forms/post_create.js';
-import { ShowError } from './error.js';
-import { Categories, showCategoryView } from './categories.js';
-import { ShowMessage } from './message.js';
-import { userLogout } from './logout.js';
-import { showPostView, displayPost, } from './posts.js';
-import { ShowUserMenu } from './user.js';
-import { attachCommentCreateListener, attachCommentEditListener } from './forms/comment_create.js';
 import { renderFooter } from './footer.js';
+import { routeSelect } from './router.js';
 import { showWelcome } from './welcome.js';
 
 const response = await fetch('/api/');
@@ -28,52 +19,8 @@ footer.classList.add('footer');
 footer.innerHTML = renderFooter(data);
 body.append(topbar, alerts, content, footer);
 
-async function routeSelect() {
-    const hash = window.location.hash;
-    const id = parseInt(hash.split('/').at(-1)) || '';
-    switch (hash) {
-    case '#/user':
-        content.innerHTML = ShowUserMenu();
-        break;
-    case '#/user/login':
-        content.innerHTML = showUserLogin();
-        attachLoginListener();
-        break;
-    case '#/user/register':
-        content.innerHTML = showUserRegister();
-        attachRegisterListener();
-        break;
-    case '#/user/logout':
-        content.innerHTML = await userLogout();
-        break;
-    case `#/category/view/${id}`:
-        content.innerHTML = await showCategoryView(id);
-        attachCommentCreateListener();
-        break;
-    case `#/post/view/${id}`:
-        content.innerHTML = await showPostView(id);
-        attachCommentCreateListener();
-        attachCommentEditListener();
-        break;
-    case `#/post/create`:
-        content.innerHTML = showPostCreateView(data);
-        attachPostCreateListener();
-        break;
-    case `#/post/edit/${id}`:
-        content.innerHTML = await showPostEditView(id);
-        attachPostCreateListener({ editing: true });
-        break;
-    case '#/' || '' || '#':
-        content.innerHTML = showWelcome(data);
-        break;
-    default:
-        content.innerHTML = showWelcome(data);
-        break;
-    }
-}
-
-routeSelect();
+routeSelect(data, content);
 
 window.addEventListener('hashchange', () => {
-    routeSelect();
+    routeSelect(data, content);
 });
---
+++ public/js/router.js
@@ -0,0 +1,57 @@
+import { showCategoryView } from './categories.js';
+import { showUserLogin, attachLoginListener } from './forms/user_login.js';
+import { showUserRegister, attachRegisterListener } from './forms/user_register.js';
+import { showPostCreateView, attachPostCreateListener, showPostEditView } from './forms/post_create.js';
+import { userLogout } from './logout.js';
+import { showPostView } from './posts.js';
+import { ShowUserMenu } from './user.js';
+import { showWelcome } from './welcome.js';
+import { attachCommentCreateListener, attachCommentEditListener } from './forms/comment_create.js';
+import { ShowError } from './error.js';
+
+export async function routeSelect(data, content) {
+    const hash = window.location.hash;
+    const id = parseInt(hash.split('/').at(-1)) || '';
+    switch (hash) {
+    case '#/user':
+        content.innerHTML = ShowUserMenu();
+        break;
+    case '#/user/login':
+        content.innerHTML = showUserLogin();
+        attachLoginListener();
+        break;
+    case '#/user/register':
+        content.innerHTML = showUserRegister();
+        attachRegisterListener();
+        break;
+    case '#/user/logout':
+        content.innerHTML = await userLogout();
+        break;
+    case `#/category/view/${id}`:
+        content.innerHTML = await showCategoryView(id);
+        attachCommentCreateListener();
+        break;
+    case `#/post/view/${id}`:
+        content.innerHTML = await showPostView(id);
+        attachCommentCreateListener();
+        attachCommentEditListener();
+        break;
+    case `#/post/create`:
+        content.innerHTML = showPostCreateView(data);
+        attachPostCreateListener();
+        break;
+    case `#/post/edit/${id}`:
+        content.innerHTML = await showPostEditView(id);
+        attachPostCreateListener({ editing: true });
+        break;
+    case '#/':
+    case '':
+    case '#':
+        content.innerHTML = showWelcome(data);
+        break;
+    default:
+        ShowError({Error:{Message:"Soft 404"}});
+        content.innerHTML = showWelcome(data);
+        break;
+    }
+}
