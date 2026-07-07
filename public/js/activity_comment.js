81599b773d8a31f9392b092a7bf690de8a93c352
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2026-07-07 19:37:21 +0300

    fixup! Moving activities

--- public/js/activity_comment.js
+++ public/js/activity_comment.js
@@ -1,16 +1,16 @@
 export function ShowActivityComment(activity) {
     return `
 <div class="container">
-    <h3>{{.Post.Title}}</h3>
-    <pre>{{.Post.Body}}</pre>
+    <h3>${activity.Post.Title}</h3>
+    <pre>${activity.Post.Body}</pre>
     <div class="activity-highlight">
-        <div class="comment" id="comment-{{.Comment.Id}}">
-            <a href="/post/view/{{.Post.Id}}#comment-{{.Comment.Id}}">
-            <pre>{{.Comment.Body}}</pre>
+        <div class="comment" id="comment-${activity.Comment.Id}">
+            <a href="/post/view/${activity.Post.Id}#comment-${activity.Comment.Id}">
+            <pre>${activity.Comment.Body}</pre>
             </a>
             <div class="reactions">
-                <button disabled>{{.Comment.Likes}} {{if .Comment.Liked}}👍{{else}}👍🏻{{end}}</button>
-                <button disabled>{{.Comment.Dislikes}} {{if .Comment.Disliked}}👎{{else}}👎🏻{{end}}</button>
+                <button disabled>${activity.Comment.Likes} ${activity.Comment.Liked?"👍":"👍🏻"}</button>
+                <button disabled>${activity.Comment.Dislikes} ${activity.Comment.Disliked?"👎":"👎🏻"}</button>
             </div>
         </div>
     </div>
