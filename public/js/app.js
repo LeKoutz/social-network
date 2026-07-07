49f999a78753d3629fe0e4cda4f00c4b77bf5aa6
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2026-07-07 17:26:39 +0300

    Detaches showWelcome() from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -10,23 +10,7 @@ import { showPostView, displayPost, } from './posts.js';
 import { ShowUserMenu } from './user.js';
 import { attachCommentCreateListener, attachCommentEditListener } from './forms/comment_create.js';
 import { renderFooter } from './footer.js';
-
-function showWelcome(data) {
-    return `
-        <div class="welcome">
-            <h2>Welcome ${(data.User.LoggedIn)? data.User.Username:''}!</h2>
-            <p>Below, you will see the post categories available as well as a brief
-            description when applicable. Click any of them to navigate to its list
-            of posts.</p>
-                ${(!data.User.LoggedIn)?
-        `<p>If you want to upload posts or comment to other ones or just react
-                (like or dislike) comments or posts, you have to <a href="#/user/login">login</a>.</p>
-                <p>In case you don't have an account, you can create one by clicking
-                <a href="#/user/register">here</a>  or via menu at the top of the page.</p>`:''}
-        </div>
-        ${Categories(data)}
-`;
-}
+import { showWelcome } from './welcome.js';
 
 const response = await fetch('/api/');
 const data = await response.json();
