5ed2340a2f3e75e09b9b5ca30c919a34f22d436b
もとない (motonai) <217199434+motonai@users.noreply.github.com>
2026-07-08 16:38:07 +0300

    Provides default values for migration 005

--- migrations/005-user-table-real-time-forum-columns-addiction.sql
+++ migrations/005-user-table-real-time-forum-columns-addiction.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
-ALTER TABLE "users" ADD COLUMN "age" INTEGER NOT NULL;
-ALTER TABLE "users" ADD COLUMN "gender" TEXT NOT NULL CHECK(gender IN ('male', 'female', 'other'));
-ALTER TABLE "users" ADD COLUMN "first_name" TEXT NOT NULL;
-ALTER TABLE "users" ADD COLUMN "last_name" TEXT NOT NULL;
+ALTER TABLE "users" ADD COLUMN "age" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "users" ADD COLUMN "gender" TEXT NOT NULL CHECK(gender IN ('male', 'female', 'other')) DEFAULT 'other';
+ALTER TABLE "users" ADD COLUMN "first_name" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "users" ADD COLUMN "last_name" TEXT NOT NULL DEFAULT '';
 END TRANSACTION;
