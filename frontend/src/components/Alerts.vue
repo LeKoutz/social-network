7880aa2a360f259579a17951e8ff830a652da434
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-02 07:58:47 +0300

    Creates components/ for .vue files that render html

---
+++ frontend/src/components/Alerts.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useAlerts } from '../composables/useAlerts.js'
+
+const { alert } = useAlerts();
+</script>
+
+<template>
+    <template v-if="alert">
+        <fieldset class="alert">
+            <legend>{{ alert.type }}</legend>
+            <p>{{ alert.message }}</p>
+        </fieldset>
+    </template>
+</template>
+
+<style scoped>
+</style>
