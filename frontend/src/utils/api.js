96eeeac3d656deb138bd5d9f8b3f2ab64d220aa4
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-02 08:42:29 +0300

    Wires everything in App.vue. Renames craeteAlert -> setAlert for consistency

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,8 +4,8 @@ export async function apiFetch(url) {
     const response = await fetch(url);
     const data = await response.json();
     if (data.Error?.Has) {
-        const { createAlert } = useAlerts()
-        createAlert(data)
+        const { setAlert } = useAlerts()
+        setAlert(data)
         return null;
     }
     return data;
