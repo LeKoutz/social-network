94a02dc14f79485ecb71523e800e0db366b868ad
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-02 08:42:29 +0300

    Wires everything in App.vue. Renames craeteAlert -> setAlert for consistency

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<!-- App.vue -->
+<script setup>
+import { ref, onMounted } from 'vue';
+import Topbar from './components/Topbar.vue';
+import Footer from './components/Footer.vue';
+import { apiFetch } from './utils/api.js';
+import Alerts from './components/Alerts.vue';
+import { useAppData } from './composables/useAppData.js';
+
+const loaded = ref(false);
+const { setAppData } = useAppData();
+
+onMounted(async () => {
+    const data = await apiFetch('/api');
+    if (data) {
+        setAppData(data);
+        loaded.value = true
+    }
+});
+</script>
 
 <template>
-    <router-view />
+    <template v-if="loaded">
+        <Topbar />
+        <Alerts />
+        <router-view />
+        <Footer />
+    </template>
 </template>
 
 <style scoped></style>
