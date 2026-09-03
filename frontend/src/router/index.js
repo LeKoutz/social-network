52e00dd49ae718eeffbf3588d21792b62f3c21d5
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-03 17:45:53 +0300

    Resolves #61 - Rewrites Registration process in Vue
    - Creates frontend/src/components/RegistrationForm.vue
    - Creates frontend/src/views/RegisterView.vue and adds it to the router
    - Retires frontend/public/js/forms/user_register.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import CategoryView from '@/views/CategoryView.vue';
 import LoginView from '@/views/LoginView.vue';
+import RegisterView from '@/views/RegisterView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
         { path: '/', component: HomeView },
         { path: '/category/view/', component: CategoryView },
         { path: '/user/login', component: LoginView },
+        { path: '/user/register', component: RegisterView },
     ],
 });
 
