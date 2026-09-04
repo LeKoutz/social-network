1a886fec21c8e2eb6b72da57b34f9f4bc1b87233
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-04 13:12:07 +0300

    Resolves #58 - Rewrite Login process in Vue
    - Creates frontend/src/components/LoginForm.vue
    - Creates frontend/src/views/LoginView.vue and adds it to the router
    - Retires frontend/public/js/components/logout.js
    - Retires frontend/public/js/forms/user_login.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import CategoryView from '@/views/CategoryView.vue';
+import LoginView from '@/views/LoginView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', component: HomeView },
         { path: '/category/view/', component: CategoryView },
+        { path: '/user/login', component: LoginView },
     ],
 });
 
